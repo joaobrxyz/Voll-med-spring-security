02ec204d7796372f6e25d82099367b38b0bef054
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-01-30 20:48:45 -0300

    Aula 12
    Utilizando um codificador de senha

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -32,5 +34,10 @@ public class ConfiguracoesSeguranca {
                         .alwaysRemember(true))
                 .csrf(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder codificadorSenha() {
+        return new BCryptPasswordEncoder();
     }
 }

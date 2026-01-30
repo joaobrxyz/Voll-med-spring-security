366234857370af69d218b7472933f534f4d5b768
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-01-30 20:35:52 -0300

    Aula 10
    Configurando a busca de usuários

---
+++ src/main/java/med/voll/web_application/domain/usuario/UsuarioService.java
@@ -0,0 +1,21 @@
+package med.voll.web_application.domain.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UsuarioService implements UserDetailsService {
+
+    private final UsuarioRepository repository;
+
+    public UsuarioService(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmailIgnoreCase(username)
+                .orElseThrow(() -> new UsernameNotFoundException("O usuário não foi encontrado"));
+    }
+}

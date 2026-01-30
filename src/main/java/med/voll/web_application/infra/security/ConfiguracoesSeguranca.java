ae28bcf817cc2f9631c8cb9ece234b1a2db71ebf
João Vitor <149846523+joaobrxyz@users.noreply.github.com>
2026-01-30 20:39:59 -0300

    Aula 11
    Alterando a validação dos usuários

--- src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
+++ src/main/java/med/voll/web_application/infra/security/ConfiguracoesSeguranca.java
@@ -16,21 +16,6 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ConfiguracoesSeguranca {
 
     @Bean
-    public UserDetailsService dadosUsuariosCadastrados(){
-        UserDetails usuario1 = User.builder()
-                .username("joao@email.com")
-                .password("{noop}joao123")
-                .build();
-        UserDetails usuario2 = User.builder()
-                .username("maria@email.com")
-                .password("{noop}maria123")
-                .build();
-        return new InMemoryUserDetailsManager(usuario1, usuario2);
-    }
-
-    // código omitido
-
-    @Bean
     public SecurityFilterChain filtrosSeguranca(HttpSecurity http) throws Exception {
         return http
                 .authorizeHttpRequests(req -> {
